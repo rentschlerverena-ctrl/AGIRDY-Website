42d5f6173f538c637401a84d2720a322f52b018c
rentschlerverena-ctrl <rentschler.verena@gmail.com>
2026-03-09 08:29:20 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Deine GitHub-URL
   site: 'https://rentschlerverena-ctrl.github.io',
-  // Der Name deines Repositories (WICHTIG!)
-  base: '/main', 
+  base: '/AGIRD-Website',
 });
